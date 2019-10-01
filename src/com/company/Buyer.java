869a3ff39487810500f1bbb991b4c4eb8627d964
Petrov <p.petrov@accenture.com>
2019-10-01 15:51:10 +0300

    Fix encapsulation

--- src/com/company/Buyer.java
+++ src/com/company/Buyer.java
@@ -4,72 +4,18 @@ import java.util.Random;
 
 public class Buyer extends Thread {
 
-    private String name;
-
     private static Shop shop;
 
-    private Random rand = new Random();
-
     public Buyer(String name, Shop shop) {
-        this.name = name;
+        super(name);
         Buyer.shop = shop;
     }
 
-    public void buy() {
-        while (true) {
-            byte fruit = (byte) (rand.nextInt((3 - 1) + 1) + 1);
-            switch (fruit) {
-                case 1:
-                    byte quantityOrange = (byte) (rand.nextInt((3 - 1) + 1) + 1);
-                    if (shop.orange > quantityOrange) {
-                        synchronized (shop) {
-                            shop.orange -= quantityOrange;
-                            System.out.println("Buyer" + this.name + "bought " + quantityOrange + " oranges.");
-                        }
-                    } else {
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    break;
-                case 2:
-                    byte quantityBanana = (byte) (rand.nextInt((3 - 1) + 1) + 1);
-                    if (shop.banan > quantityBanana) {
-                        synchronized (shop) {
-                            shop.banan -= quantityBanana;
-                            System.out.println("Buyer" + this.name + "bought " + quantityBanana + " bananas.");
-                        }
-                    } else {
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    break;
-                case 3:
-                    byte quantityApple = (byte) (rand.nextInt((3 - 1) + 1) + 1);
-                    if (shop.apple > quantityApple) {
-                        synchronized (shop) {
-                            shop.apple -= quantityApple;
-                            System.out.println("Buyer" + this.name + "bought " + quantityApple + " apples.");
-                        }
-                    } else {
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    break;
-            }
-        }
-    }
 
     public void run() {
-        buy();
+        while (true) {
+            shop.buy();
+        }
     }
 
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String args[]){
 
         Shop pernikGlaven = new Shop();
-        Deliver vankata = new Deliver("VANKATA", pernikGlaven);
-        Deliver pepi = new Deliver("PEPI", pernikGlaven);
-        Buyer nikolinka = new Buyer("Nikolinka", pernikGlaven);
-        Buyer stanoicho = new Buyer("stanoicho", pernikGlaven);
+        Deliver vankata = new Deliver("Magitoooo da se ne surdi",pernikGlaven);
+        Deliver pepi = new Deliver("TOQ E ZLE", pernikGlaven);
+        Buyer nikolinka = new Buyer("tonkata", pernikGlaven);
+        Buyer stanoicho = new Buyer("antoncho", pernikGlaven);
 
 
         vankata.start();
